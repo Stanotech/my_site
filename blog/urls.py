b496be5aaa4a347ad952259a01f71b1a57696a10
Sta Rog <satach@o2.pl>
2022-10-21 02:42:21 +0200

    base template

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
-    # path("posts", views.posts, name='posts'),
-    # path("posts/<slug:slug>", views.post_detail, name='post_detail')
+    path("posts", views.posts, name='posts'),
+    path("posts/<slug:slug>", views.single_post, name='single_post')
 
 ]
