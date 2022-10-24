57a7fbc3ffcc8faa953d68da215a75cdae07a886
Sta Rog <satach@o2.pl>
2022-10-24 22:00:57 +0200

    dynamic links

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts", views.posts, name='posts'),
-    path("posts/<slug:slug>", views.single_post, name='single_post')
+print("zduoy")
 
+urlpatterns = [
+    path("", views.starting_page, name="starting_page"),
+    path("posts", views.posts, name="posts"),
+    path("posts/<slug:slug>", views.single_post, name="single_post") #parametr slug przekazywany do funckji
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+posts_content = {
+
+    "post1": {
+        "title": "Mountains",
+        "photo": "fsdfdsa/sdafas",
+        "content": "FAJNIE SIE CHODZI"
+
+    },
+    "post2": {
+        "title": "shopping",
+        "photo": "fsdsdsa/sdafas",
+        "content": "FAJNIE SIE kupuje"
+
+    },    
+}
+
 def starting_page(request):
     return render(request, "blog/index.html")
 
 def posts(request):
     return render(request, "blog/all_posts.html")
 
-def single_post(request):
-    return render(request, "blog/single-post.html")
+def single_post(request, slug):
+    
+    try:
+        print("kapaciapa")
+        print(posts_content[slug]["title"])
+        return render(request, "blog/single_post.html", {
+            "title": posts_content[slug]["title"],
+            "photo": posts_content[slug]["photo"],
+            "content": posts_content[slug]["content"],            
+        })
 
- 
+    except:
+        raise Http404("Site does not exist")
