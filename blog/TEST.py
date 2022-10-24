204303acfb00637520e383b58c36d0cf909d8f56
Sta Rog <satach@o2.pl>
2022-10-24 22:00:57 +0200

    dynamic links

---
+++ blog/TEST.py
@@ -0,0 +1,20 @@
+posts = {
+
+    "post1": {
+        "title": "Mountains",
+        "photo": "fsdfdsa/sdafas",
+        "content": "FAJNIE SIE CHODZI"
+
+    },
+    "post1": {
+        "title": "shopping",
+        "photo": "fsdsdsa/sdafas",
+        "content": "FAJNIE SIE kupuje"
+
+    },    
+}
+
+
+zmienna = "post1"
+
+print(posts[zmienna]["title"])
