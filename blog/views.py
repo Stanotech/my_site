6a2ce6923e159d17b2bf79e202e0815341a235d9
Sta Rog <satach@o2.pl>
2022-10-26 01:34:23 +0200

    content update

--- blog/views.py
+++ blog/views.py
@@ -22,18 +22,27 @@ posts_content = {
 
     },    
     "post3": {
-        "title": "How to become a Python Web Developer",
+        "title": "Your father annoy you ? How to get rid of him",
         "image": "nerd.jpg",
         "author": "Stan",
         "date": date(2022, 10, 21),
         "excerpt": "Few secret simple tricks for becoming Web Dev in just a few clicks ! HR Department hates me! ",
         "content": "You just need a GitHub with projects like this: <ul> <li> Vending MAchine</li> <li> Online game </li>"
-
+    },   
+    "post4": {
+        "title": "How we eat is very important",
+        "image": "nerd.jpg",
+        "author": "Stan",
+        "date": date(2022, 10, 21),
+        "excerpt": "Few secret simple tricks for becoming Web Dev in just a few clicks ! HR Department hates me! ",
+        "content": "You just need a GitHub with projects like this: <ul> <li> Vending MAchine</li> <li> Online game </li>"
     },   
 }
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    return render(request, "blog/index.html", {
+            "all_posts": posts_content,             #passing all post(posts_content) variable to index.htmp
+        })           
 
 def posts(request):
     return render(request, "blog/all_posts.html")
@@ -41,11 +50,12 @@ def posts(request):
 def single_post(request, slug):
     
     try:
-        print("kapaciapa")
-        print(posts_content[slug]["title"])
         return render(request, "blog/single_post.html", {
             "title": posts_content[slug]["title"],
             "image": posts_content[slug]["image"],
+            "author": posts_content[slug]["author"],
+            "date": posts_content[slug]["date"],
+            "excerpt": posts_content[slug]["excerpt"],
             "content": posts_content[slug]["content"],            
         })
 
