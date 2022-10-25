62f14fe1dcfc1ef76791719352dd93cf211accba
Sta Rog <satach@o2.pl>
2022-10-25 17:21:21 +0200

    content creating

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,35 @@
+from datetime import date
 from django.shortcuts import render
 from django.http import Http404
 
 posts_content = {
 
     "post1": {
-        "title": "Mountains",
-        "photo": "fsdfdsa/sdafas",
-        "content": "FAJNIE SIE CHODZI"
-
+        "title": "How to become a Python Web Developer",
+        "image": "nerd.jpg",
+        "author": "Stan",
+        "date": date(2022, 10, 24),
+        "excerpt": "Few secret simple tricks for becoming Web Dev in just a few clicks ! HR Department hates me! ",
+        "content": "You just need a GitHub with projects like this: <ul> <li> Vending MAchine</li> <li> Online game </li>"
     },
     "post2": {
-        "title": "shopping",
-        "photo": "fsdsdsa/sdafas",
-        "content": "FAJNIE SIE kupuje"
+        "title": "How to become a Python Web Developer",
+        "image": "nerd.jpg",
+        "author": "Stan",
+        "date": date(2022, 10, 27),
+        "excerpt": "Few secret simple tricks for becoming Web Dev in just a few clicks ! HR Department hates me! ",
+        "content": "You just need a GitHub with projects like this: <ul> <li> Vending MAchine</li> <li> Online game </li>"
 
     },    
+    "post3": {
+        "title": "How to become a Python Web Developer",
+        "image": "nerd.jpg",
+        "author": "Stan",
+        "date": date(2022, 10, 21),
+        "excerpt": "Few secret simple tricks for becoming Web Dev in just a few clicks ! HR Department hates me! ",
+        "content": "You just need a GitHub with projects like this: <ul> <li> Vending MAchine</li> <li> Online game </li>"
+
+    },   
 }
 
 def starting_page(request):
@@ -30,7 +45,7 @@ def single_post(request, slug):
         print(posts_content[slug]["title"])
         return render(request, "blog/single_post.html", {
             "title": posts_content[slug]["title"],
-            "photo": posts_content[slug]["photo"],
+            "image": posts_content[slug]["image"],
             "content": posts_content[slug]["content"],            
         })
 
