ac271f759368f9b47403d53412a67d3462e3a42e
Sta Rog <satach@o2.pl>
2022-10-29 03:37:32 +0200

    working on all post page

--- blog/views.py
+++ blog/views.py
@@ -53,14 +53,16 @@ def starting_page(request):
         })           
 
 def posts(request):
-    return render(request, "blog/all_posts.html")
-
+    posts_content.sort(key=get_date)
+    latest_posts = posts_content[-4:]
+    return render(request, "blog/all_posts.html", {
+            "all_posts": latest_posts,             #passing all post(posts_content) variable to index.htmp
+        })       
+        
 def single_post(request, slug):
     wanted_post = next(post for post in posts_content if slug == post["slug"])  #if requested slug is equal to slug of post from dictionary than assaign this post to
     
     try:
-        print("gowno")
-        print(wanted_post)
         return render(request, "blog/single_post.html", {
             "title": wanted_post["title"],
             "image": wanted_post["image"],
