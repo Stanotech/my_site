819f39e72f6c430fd0cb9e6f5066fdec9fb64a59
Sta Rog <satach@o2.pl>
2022-11-22 01:35:17 +0100

    before migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+     first_name =models.CharField(max_length=15)
+     last_name =models.CharField(max_length=15)
+     email =models.CharField(max_length=20)
+
+class Tag(models.Model):
+     caption =models.CharField(max_length=8)
+
+class Post(models.Model):
+     slug =models.CharField(max_length=8)
+     title =models.CharField(max_length=20)
+     image =models.CharField(max_length=30)
+     author =models.ForeignKey(Author, on_delete=models.CASCADE)
+     date = models.DateField()
+     excerpt =models.CharField(max_length=100)
+     content =models.CharField(max_length=2000)
+     tag = models.ManyToManyField(Tag)
+     
+
+
+
