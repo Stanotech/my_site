bacdc7bae425b046d3e9eb35a5b3e91f000412ed
Sta Rog <satach@o2.pl>
2022-12-13 10:58:28 +0100

    Before forms,sesssions, uploads

--- blog/views.py
+++ blog/views.py
@@ -3,74 +3,33 @@ from django.shortcuts import render
 from django.http import Http404
 from .models import Author, Post, Tag
 
-posts_content = [
-    {
-        "slug": "post1",
-        "title": "How to become a Python Web Developer",
-        "image": "nerd.jpg",
-        "author": "Stan",
-        "date": date(2022, 10, 24),
-        "excerpt": "Few secret simple tricks for becoming Web Dev in just a few clicks ! HR Department hates me! ",
-        "content": "You just need a GitHub repository with projects like this: <ul> <li> Vending machine done entirely by yourself</li> <li> Online pong game </li> <li> Blog site made with Django framework </li> <li> G-code generator for smoothing artefacts in your 3D prints </li></ul>"
-    },
-    {
-        "slug": "post2",
-        "title": "Think before you do something",
-        "image": "passion.jpg",
-        "author": "Stan",
-        "date": date(2022, 10, 27),
-        "excerpt": "Some people say that our brains is for thinking, but American scientist say- Netflix is better",
-        "content": "Your passion is right in front of your face. If you have to look for it, then youre probably not passionate about it at all. So screw finding your passion."
+posts_content = Post.objects.all().order_by("date")
 
-    },    
-    {
-        "slug": "post3",
-        "title": "Should we use violence in raising parents ?",
-        "image": "post-slide-4.jpg",
-        "author": "Bobo",
-        "date": date(2022, 10, 21),
-        "excerpt": "New research shows that violence is a good way to convince parents to whatever you want",
-        "content": "Just a few ideas for all kids: Brazen bull, Breaking wheel, Breast ripper, Batog, Birch rod, Catapelta[citation needed], Choke, pear, Cattle, prod, Electroshock, weapon, Gag, Garrote, Graduated, Electronic, Decelerator, Head, crusher, Instep, borer, Iron, chair, Iron, Maiden, (use, is, debated), Jiagun, /, Zanzhi, Judas, cradle, Knee, splitter, Led, sprinkler, Mancuerda, Parrilla, Pau, de, Arara, Pendulum[2], (of, disputed, historicity), Picana, Pillory, Rack, Rope, Scavenger's, daughter, Scold's, bridle, Spanish, boot, Strappado, Stocks, Tablilla, Thumbscrew, Tongue, tearer, Tramp, chair, Tucker, telephone, Whip Wooden horse"
-    },   
-    {
-        "slug": "post4",
-        "title": "How we eat is very important",
-        "image": "monkey.jpg",
-        "author": "Stan",
-        "date": date(2022, 10, 20),
-        "excerpt": "You are what you eat",
-        "content": "If you eat a coconut, you will become a coconut. That's it."
-    },   
-]
-
-def get_date(post):
-    return post['date']
 
 def starting_page(request):
-    posts_content.sort(key=get_date)
-    latest_posts = posts_content[-4:]
+    latest_posts = posts_content
     return render(request, "blog/index.html", {
             "all_posts": latest_posts,             #passing all post(posts_content) variable to index.htmp
         })           
 
 def posts(request):
-    posts_content.sort(key=get_date)
-    latest_posts = posts_content[-4:]
+    latest_posts = posts_content
     return render(request, "blog/all_posts.html", {
             "all_posts": latest_posts,             #passing all post(posts_content) variable to index.htmp
         })       
         
 def single_post(request, slug):
-    wanted_post = next(post for post in posts_content if slug == post["slug"])  #if requested slug is equal to slug of post from dictionary than assaign this post to
-    
+    print(slug)
+    wanted_post = next(post for post in posts_content if slug == post.slug)  #if requested slug is equal to slug of post from dictionary than assaign this post. Next convert last list object to just object outside list.
+    print(wanted_post)
     try:
         return render(request, "blog/single_post.html", {
-            "title": wanted_post["title"],
-            "image": wanted_post["image"],
-            "author": wanted_post["author"],
-            "date": wanted_post["date"],
-            "excerpt": wanted_post["excerpt"],
-            "content": wanted_post["content"],            
+            "title": wanted_post.title,
+            "image": wanted_post.image,
+            "author": wanted_post.author,
+            "date": wanted_post.date,
+            "excerpt": wanted_post.excerpt,
+            "content": wanted_post.content,            
         })
 
     except:
