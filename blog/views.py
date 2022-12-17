7f37855341dd006f8a15eedc99dffd5c2ad3d67b
Sta Rog <satach@o2.pl>
2022-12-17 18:22:40 +0100

    before editing detailpost

--- blog/views.py
+++ blog/views.py
@@ -3,34 +3,42 @@ from django.shortcuts import render
 from django.http import Http404
 from .models import Post, Comment
 from .forms import CommentForm
-from django.views.generic.detail import DetailView
-from django.views.generic import ListView
+
+from django.views.generic import CreateView, TemplateView, ListView
 
 posts_content = Post.objects.all().order_by("date")
 
 
-def starting_page(request):
-    latest_posts = posts_content
-    return render(request, "blog/index.html", {
-        # passing all post(posts_content) variable to index.htmp
-        "all_posts": latest_posts,
-    })
-
-
-def posts(request):
-    latest_posts = posts_content
-    return render(request, "blog/all_posts.html", {
-        # passing all post(posts_content) variable to index.htmp
-        "all_posts": latest_posts,
-    })
-
-
-class single_post(DetailView):
-    template_name = "blog/single_post.html"
-    model = Post
+class starting_page(TemplateView):
+    template_name = "blog/index.html"
 
     def get_context_data(self, **kwargs):
-        wanted_post = next(post for post in posts_content if self.kwargs["slug"]== post.slug)
+        context = super().get_context_data(**kwargs)
+        context["all_posts"] = posts_content
+        return context
+
+
+class posts(ListView):
+    template_name = "blog/all_posts.html"
+    model = Post
+    context_object_name = "all_posts"
+    ordering = ["-date"]
+
+
+class single_post(CreateView):
+    template_name = "blog/single_post.html"
+    model = Post
+    form_class = CommentForm
+    succes_url = "blog/single_post.html"
+
+    # by default view don’t know what to do with data.
+    def form_valid(self, form):
+        form.save()				# so please save data to database
+        return super().form_valid(form)  # and return it
+
+    def get_context_data(self, **kwargs):
+        wanted_post = next(
+            post for post in posts_content if self.kwargs["slug"] == post.slug)
         context = super().get_context_data(**kwargs)
         context["title"] = wanted_post.title
         context["image"] = wanted_post.image
@@ -40,5 +48,5 @@ class single_post(DetailView):
         context["content"] = wanted_post.content
         context["comments"] = Comment.objects.all()
         context["form"] = CommentForm()
-        
+
         return context
