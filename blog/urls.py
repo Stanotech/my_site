5c69664fc81ba7cbdb49b3d5c1fcf06f2a98f8ab
Sta Rog <satach@o2.pl>
2023-04-20 11:29:26 +0200

    style correction PEP8

--- blog/urls.py
+++ blog/urls.py
@@ -5,11 +5,11 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page.as_view(), name="starting_page"),
-    path("posts", views.posts.as_view(), name="posts"),
-    path("about", views.about.as_view(), name="about"),
-    path("contact", views.contact.as_view(), name="contact"),
-    path("posts/to_read", views.posts_to_read.as_view(), name="to_read"),
+    path("", views.StartingPage.as_view(), name="starting_page"),
+    path("posts", views.Posts.as_view(), name="posts"),
+    path("about", views.About.as_view(), name="about"),
+    path("contact", views.Contact.as_view(), name="contact"),
+    path("posts/to_read", views.PostsToRead.as_view(), name="to_read"),
     path("posts/send", views.AddToRead.as_view(), name="session"), #parametr slug przekazywany do funckji
-    path("posts/<slug:slug>", views.single_post.as_view(), name="single_post"), #parametr slug przekazywany do funckji
+    path("posts/<slug:slug>", views.SinglePost.as_view(), name="single_post"), #parametr slug przekazywany do funckji
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,14 @@
 from datetime import date
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
+from django.views.generic import CreateView, TemplateView, ListView, View
 from .models import Post, Comment
 from .forms import CommentForm
 
-from django.views.generic import CreateView, TemplateView, ListView
-from django.views import View
-
 posts_content = Post.objects.all().order_by("date")
 
-class starting_page(TemplateView):
+
+class StartingPage(TemplateView):
     template_name = "blog/index.html"
 
     def get_context_data(self, **kwargs):
@@ -17,34 +16,36 @@ class starting_page(TemplateView):
         context["all_posts"] = posts_content
         return context
 
-class about(TemplateView):
+
+class About(TemplateView):
     template_name = "blog/about.html"
 
-class contact(TemplateView):
+
+class Contact(TemplateView):
     template_name = "blog/contact.html"
 
 
-class posts(ListView):
+class Posts(ListView):
     template_name = "blog/all_posts.html"
     model = Post
     context_object_name = "all_posts"
     ordering = ["-date"]
 
 
-class posts_to_read(View):
+class PostsToRead(View):
     def get(self, request):
-        stored_post_id_list = request.session.get("read_later")    #read session variable from database
+        stored_post_id_list = request.session.get("read_later")  # read session variable from database
         if stored_post_id_list is None:
             all_posts = "empty"
         else:
-            all_posts = Post.objects.filter(pk__in=stored_post_id_list) 
+            all_posts = Post.objects.filter(pk__in=stored_post_id_list)
 
         return render(request, "blog/to_read_list.html", {
             "all_posts": all_posts
         })
 
 
-class single_post(View):
+class SinglePost(View):
 
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
@@ -59,31 +60,31 @@ class single_post(View):
             "post": post,
             "post_tags": post.tag.all(),
             "comments": Comment.objects.filter(post=post),
-            "form": CommentForm(),            
+            "form": CommentForm(),
             "slug": slug,
             "read_later": read_later,
-            
+
         }
         return render(request, "blog/single_post.html", context)
 
-    def post(self, request, slug):     
+    def post(self, request, slug):
         form = CommentForm(request.POST)
         post = Post.objects.get(slug=slug)
         context = {
             "post": post,
             "post_tags": post.tag.all(),
             "comments": Comment.objects.filter(post=post),
-            "form": CommentForm(),            
+            "form": CommentForm(),
             "slug": slug,
-            
+
         }
 
-        if form.is_valid():                     # is user empty ?, id form is valid ?,
-            comment = form.save(commit=False)   # creating form object to edit
-                                                # editing the "post" field to let database know to which post it should be saved
+        if form.is_valid():  # is user empty ?, id form is valid ?,
+            comment = form.save(commit=False)  # creating form object to edit
+            # editing the "post" field to let database know to which post it should be saved
             comment.post = post
             comment.save()
-                                                # if not empty than redirect to thank you url
+            # if not empty than redirect to thank you url
             return render(request, "blog/single_post.html", context)
 
         return render(request, "blog/single_post.html", context)
@@ -91,14 +92,14 @@ class single_post(View):
 
 class AddToRead(View):
     def post(self, request):
-        stored_posts = request.session.get("read_later")            #read session variable from database
-        if stored_posts is None:                                    #first creation of empty list if there is no variable in database
-            stored_posts = []      
-        post_id = int(request.POST['post_id'])                      #getting the post id from request
+        stored_posts = request.session.get("read_later")  # read session variable from database
+        if stored_posts is None:  # first creation of empty list if there is no variable in database
+            stored_posts = []
+        post_id = int(request.POST['post_id'])  # getting the post id from request
         if post_id not in stored_posts:
-            stored_posts.append(post_id)                            # add post id to list it it's not there yet
+            stored_posts.append(post_id)  # add post id to list it it's not there yet
         else:
-            stored_posts.remove(post_id)                            # or remove it if it is there allready
-        request.session["read_later"] = stored_posts                # save this list to session variable in database
+            stored_posts.remove(post_id)  # or remove it if it is there allready
+        request.session["read_later"] = stored_posts  # save this list to session variable in database
         current_post = Post.objects.get(id=post_id)                 # getting the post of that ID to get the slug
         return HttpResponseRedirect("/posts/" + current_post.slug)
